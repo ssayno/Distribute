d91aa1c3cad7dde2cc53248fdea38c93be26bf69
gl20227 <418215971@qq.com>
2022-10-28 14:08:12 +0800

    finished all func

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
+from os.path import isdir
 import sys
 import os
 import json
-from PyQt5.QtCore import QPoint, QRectF, QThread, QTimer, Qt, pyqtSignal
-from PyQt5.QtGui import QCloseEvent, QCursor, QPainterPath, QRegion
-from PyQt5.QtWidgets import QDialog, QLabel, QMainWindow, QApplication, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtCore import QCalendar, QDateTime, QPoint, QRectF, QThread, QTimer, Qt, pyqtSignal
+from PyQt5.QtGui import QCloseEvent, QCursor, QFont, QPainterPath, QRegion
+from PyQt5.QtWidgets import QDialog, QHeaderView, QLabel, QMainWindow, QApplication, QFileDialog, QTreeWidget, QTreeWidgetItem, QVBoxLayout, QWidget
 from UiWidgets.mainui import UIWidget
-from settings import DELIMITER, TOKEN_SIZE, TIMER_GAP
+from settings import DELIMITER, TOKEN_SIZE, TIMER_GAP, NAME_DELIMITER
 from Utils.config import Config
 import qdarkstyle
 import fileinput
 import re
+import glob
+import shutil
 
 
 class Distribute_Command(QMainWindow):
     def __init__(self) -> None:
         super(Distribute_Command, self).__init__()
-        self.setFixedSize(600, 350)
+        self.setFixedSize(800, 350)
         path = QPainterPath()
         path.addRoundedRect(QRectF(self.rect()), 10, 10)
         mask = QRegion(path.toFillPolygon().toPolygon())
@@ -43,6 +46,7 @@ class Distribute_Command(QMainWindow):
         # title bar button
         self.cw.title_bar.close_button.clicked.connect(
             self.quit_this_app
+
         )
         self.cw.title_bar.minimum_button.clicked.connect(
             self.titlebar_minimize_app
@@ -92,16 +96,17 @@ class Distribute_Command(QMainWindow):
         else:
             self.cw.start_button.setEnabled(False)
             split_token_thread = Split_Token(
-                distribute_path=distribute_path, input_path=input_path, command=command, token_size=token_size,
-                delimiter=delimiter, parent=self
+                distribute_path=distribute_path, input_path=input_path, command=command, dialong_update_signal=self.dialog.update_tree_signal,
+                token_size=token_size, delimiter=delimiter, parent=self
             )
             split_token_thread.start()
-            # tim = QTimer(self)
-            # tim.timeout.connect(lambda: print(split_token_thread.isFinished()))
-            # tim.start(1000)
             split_token_thread.finished.connect(
-                lambda: self.dialog._timer.start(TIMER_GAP)
+                lambda: self.connect_and_start_dialog_timer(distribute_path)
             )
+
+    def connect_and_start_dialog_timer(self, d_path):
+        self.dialog.listened_path = d_path
+        self.dialog._timer.start(TIMER_GAP)
 
     def mousePressEvent(self, event):
         self.oldPos = event.globalPos()
@@ -153,15 +158,21 @@ class Distribute_Command(QMainWindow):
 
 class Process_dialog(QDialog):
     follow_signal = pyqtSignal()
+    update_tree_signal = pyqtSignal(str, str, str, bool)
 
     def __init__(self, parent=None):
         super(Process_dialog, self).__init__(parent=parent)
         # self.setStyleSheet("QDialog{background-color: red;}")
-        # self.setStyleSheet('QDialog{border: solid 2px white; border-radius: 20px}')
+        self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
         # set QDialog size
         self.w_width = 400
         self.w_height = 600
         self.radius = 20
+        self.resize(self.w_width, self.w_height)
+        path = QPainterPath()
+        path.addRoundedRect(QRectF(self.rect()), self.radius, self.radius)
+        mask = QRegion(path.toFillPolygon().toPolygon())
+        self.setMask(mask)
         #
         self.q_layout = QVBoxLayout()
         self.q_layout.setAlignment(Qt.AlignTop)
@@ -172,40 +183,119 @@ class Process_dialog(QDialog):
         self.addUIWidget()
         # timer
         self._timer = QTimer(self)
-        self._timer.timeout.connect(self.update_timer_func)
+        # connect signal with slot func
+        self.connect_signal_with_slot_func()
+        # need a map represent the parent item
+        self.map_name_item = {}
+        self.listened_path = None
 
     def addUIWidget(self):
         banner = QLabel("动态显示")
         banner.setAlignment(Qt.AlignCenter)
         self.q_layout.addWidget(banner)
-        self.countLabel = QLabel('0')
-        self.q_layout.addWidget(self.countLabel)
+        self.timeLabel = QLabel("Not Start")
+        self.timeLabel.setAlignment(Qt.AlignCenter)
+        self.q_layout.addWidget(self.timeLabel)
+        # tree show
+        self.dir_tree = QTreeWidget(self)
+        # basic configuration for tree dir
+        self.dir_tree.setHeaderLabels(["Dir Or File", "Status"])
+        self.dir_tree.header().setDefaultAlignment(Qt.AlignCenter)
+        self.dir_tree.header().setSectionResizeMode(QHeaderView.Stretch)
+        # dir tree qss style
+        self.dir_tree.setStyleSheet("QTreeWidget{font: 16px;} QTreeWidget QHeaderView:section{font-size: 16px;}")
+        # self.dir_tree_root = QTreeWidgetItem(self.dir_tree)
+        # self.dir_tree_root.setTextAlignment(Qt.AlignCenter, Qt.AlignCenter)
+        # self.dir_tree_root.setText(0, "aaa")
+        # self.dir_tree_root.setText(1, "Normal")
+        self.q_layout.addWidget(self.dir_tree)
 
     def _setPosition(self):
         left = self.parent().x() + self.parent().width()
-        top = self.parent().y() - self.w_height / 2 + self.parent().height() / 2
+        top = self.parent().y()
         self.setGeometry(
             int(left), int(top), self.w_width, self.w_height
         )
-        path = QPainterPath()
-        path.addRoundedRect(QRectF(self.rect()), self.radius, self.radius)
-        mask = QRegion(path.toFillPolygon().toPolygon())
-        self.setMask(mask)
 
-    def update_timer_func(self):
-        new_value = int(self.countLabel.text()) + 1
-        self.countLabel.setText(f'{new_value}')
+    def update_time_func(self):
+        time_ = QDateTime.currentDateTime()
+        format_time_ = time_.toString("yyyy-MM-dd hh:mm:ss dddd")
+        self.timeLabel.setText(f'{format_time_}')
+        # first item
+        first_item = self.dir_tree.itemAt(0, 0)
+        self.dir_tree.setCurrentItem(first_item)
+        if self.dir_tree.itemsExpandable():
+            self.dir_tree.expandItem(first_item)
+
 
     def show(self) -> None:
         self.follow_signal.emit()
         return super().show()
+
+    def connect_signal_with_slot_func(self):
+        print('connect slot func')
+        self.update_tree_signal.connect(self.update_tree_widget)
+        self._timer.timeout.connect(self.listen_dir)
+
+    def update_tree_widget(self, name, status, company_name, modify):
+        if modify:
+            need_modify_item = self.map_name_item[company_name]['JSON-FILES'].get(name, None)
+            if need_modify_item is None:
+                print("没有这个节点")
+                return
+            old_status = need_modify_item.text(1)
+            print(old_status)
+            if old_status != status:
+                need_modify_item.setText(1, status)
+        else:
+            if company_name not in self.map_name_item:
+                header_item = QTreeWidgetItem(self.dir_tree)
+                header_item.setText(0, company_name)
+                header_item.setText(1, "Pending")
+                self.map_name_item[company_name] = {"company_name": header_item}
+            parent = self.map_name_item[company_name]["company_name"]
+            name_item_widget = QTreeWidgetItem(parent)
+            name_item_widget.setText(0, name)
+            name_item_widget.setText(1, status)
+            if self.map_name_item[company_name].get("JSON-FILES", None) is None:
+                self.map_name_item[company_name]['JSON-FILES'] = {
+                    name: name_item_widget
+                }
+            else:
+                self.map_name_item[company_name]['JSON-FILES'][name] = name_item_widget
+
+    def listen_dir(self):
+        # update time
+        self.update_time_func()
+        if self.listened_path is None or not os.path.isdir(self.listened_path):
+            print('Listen Dir is invalid')
+            return
+        all_json_file_path = glob.glob(self.listened_path+ "/**/*.json", recursive=True)
+        for json_file in all_json_file_path:
+            temp_list = json_file.split(os.sep)
+            company_name = temp_list[-2]
+            if 'running' in company_name or 'done' in company_name:
+                continue
+            json_file_name = temp_list[-1]
+            if 'DONE' in json_file_name:
+                origin_name = f"{json_file_name.split(NAME_DELIMITER)[0]}.json"
+                company_done_path = os.path.join(os.sep.join(temp_list[:-2]), f'{company_name}-done')
+                if not os.path.exists(company_done_path):
+                    os.mkdir(company_done_path)
+                shutil.move(json_file, company_done_path)
+                self.update_tree_signal.emit(origin_name, "DONE", company_name, True)
+                print('done')
+            elif NAME_DELIMITER in json_file_name:
+                origin_name = f"{json_file_name.split(NAME_DELIMITER)[0]}.json"
+                self.update_tree_signal.emit(origin_name, "Running", company_name, True)
+
 
 class Test(QWidget):
     def __init__(self):
         super(Test, self).__init__()
         tim = QTimer(self)
         # single_ = QTestThread(self, "", "", "")
-        single_ = Split_Token("", "", "")
+        single_ = Split_Token("", "", "", "")
         single_.start()
         tim.timeout.connect(lambda: print(single_.isFinished()))
         tim.start(2000)
@@ -223,13 +313,14 @@ class QTestThread(QThread):
         print('a')
 
 class Split_Token(QThread):
-    def __init__(self, distribute_path, input_path, command, token_size=TOKEN_SIZE, delimiter=DELIMITER, parent=None):
+    def __init__(self, distribute_path, input_path, command, dialong_update_signal, token_size=TOKEN_SIZE, delimiter=DELIMITER, parent=None):
         super(Split_Token, self).__init__(parent=parent)
         self.dp = distribute_path
         self.ip = input_path
         self.tk_size = token_size
         self.delimiter = delimiter
         self.command = command
+        self.dus = dialong_update_signal
 
     def run(self) -> None:
         try:
@@ -252,6 +343,8 @@ class Split_Token(QThread):
                     image_dst_distributed = content['distination']
                     passed_command = f'{command} --file-list "{file_list_argument}" --image-dst-distributed "{image_dst_distributed}"'
                     json.dump({'command': passed_command}, f, indent='\t', ensure_ascii=False)
+                temp_list = single_config_path.split(os.sep)
+                self.dus.emit(temp_list[-1], 'Pending', temp_list[-2], False)
         except Exception as e:
             print(e)
         finally:
--- settings.py
+++ settings.py
@@ -10,9 +10,10 @@ COOKIES = os.path.normpath(
         os.path.dirname(__file__), 'Data', 'Cookies'
     )
 )
+NAME_DELIMITER = "___"
 TIMER_GAP = 1000
-DELIMITER = '''||'''
+DELIMITER = '''|'''
 TOKEN_SIZE = 90
 COMMAND = '''python translate_simple-v3.py --use-ctd --filter-keywords "佛山,赤语,chiy,服装店,伊山,售后,含税,拿样,是否定制,选择我们,义乌,加工,定制,开票价格,我们的优势,实力商家,招牌,清仓,分销,现货,贴牌,定制,1688,taobao,ODM,工厂,.com" --use-inpainting --use-cuda --force-horizontal  --manga2eng --translator deepl --mode distributed'''
-DISTRIBUTE_PATH = '''/Users/guoliang/Desktop/佛山市赤语服饰有限公司/'''
-INPUT_PATH = '''/Users/guoliang/Desktop/blacklist_pic/'''
+DISTRIBUTE_PATH = '''/Users/guoliang/Desktop/Distribute'''
+INPUT_PATH = '''/Users/guoliang/Desktop/佛山市赤语服饰有限公司'''
